86e7619ce2407220d1d502a69218dfe04a7a1466
golars497 <john.calzado497@gmail.com>
2024-05-05 22:26:22 +0100

    update

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+last_msg = ""
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return last_msg
+    # return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/trade")
+def trade(request):
+    last_msg = request
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
