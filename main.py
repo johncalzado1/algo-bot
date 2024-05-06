eacd03c66b8a7f8d94c544a723e3b59557651c31
golars497 <john.calzado497@gmail.com>
2024-05-06 20:10:04 +0100

    major update

--- main.py
+++ main.py
@@ -2,17 +2,104 @@ from typing import Union
 from pprint import pprint
 from fastapi import FastAPI
 from pydantic import BaseModel
+import time, hashlib, time, hmac, base64, requests, json
 
 app = FastAPI()
 app.last_msg = None
 
 
+# app.api_key = "6638fd9e3b72770001540b3c"  # spot
+# app.api_secret = "1942be0f-6f37-4192-89b5-f1ae751bab93"  # spot
+app.api_key = "6638107e411f5a0001768136"
+app.api_secret = "903ddafa-5397-45ee-b849-ff203328b133"
+app.api_passphrase = "lyndon77"
+# app.base_url = "https://api.kucoin.com/api/v1/orders"  # spot
+app.base_url = 'https://api-futures.kucoin.com' # futures 
+# app.base_url = 'https://api-futures.kucoin.com/api/v1/position?symbol=XBTUSDM'
+
+
 class TradeRequest(BaseModel):
     msg: str
-    side: str # long /short
-    price: str = None
+    price: str
+    size_usdt: int
+    action: str # long, short, close
 
 
+def get_time():
+    return int(time.time() * 1000)
+
+def get_headers(_str_to_sign):
+    signature = base64.b64encode(
+        hmac.new(app.api_secret.encode('utf-8'), _str_to_sign.encode('utf-8'), hashlib.sha256).digest())
+    passphrase = base64.b64encode(hmac.new(app.api_secret.encode('utf-8'), app.api_passphrase.encode('utf-8'), hashlib.sha256).digest())
+    return {
+        'Content-type':'application/json', 
+        'Accept':'application/json',
+        "KC-API-SIGN": signature,
+        "KC-API-TIMESTAMP": str(get_time()),
+        "KC-API-KEY": app.api_key,
+        "KC-API-PASSPHRASE": passphrase,
+        "KC-API-KEY-VERSION": "2"
+    }
+
+
+def trade(side, size_usdt, stopType=None, stopPrice=None, stopPriceType="TP", type="market"):
+    """Opens a position
+    side (str): "buy" or "sell"
+    size_usdt (int): 
+    type (str): "market" or "limit"
+    stopType (str): up (for shorts) OR down (for longs)
+    """
+    
+    data = { # futures
+        "clientOid": "jc-trade",
+        "side": side,
+        "symbol": "XBTUSDTM", 
+        "leverage": "125", 
+        "type": "market", 
+        "size": size_usdt,
+    } 
+
+    if "stop" != None:
+        data.update({
+            "stop": stopType,
+            "stopPrice": stopPrice,
+            "stopPriceType": stopPriceType
+        })
+
+    data_json = json.dumps(data)
+    
+    url = app.base_url + '/api/v1/orders'
+    str_to_sign = str(get_time()) + 'POST' + "/api/v1/orders" + data_json
+
+    headers = get_headers(str_to_sign)
+
+    response = requests.request("post", url=url, headers=headers, data=data_json)
+    print(response.status_code)
+    print(response.json())
+
+
+
+def close_all_pos():
+    """Close all trades
+    """
+    data = {
+        "clientOid": "jc-trade",
+        "symbol": "XBTUSDTM", 
+        "type": "market",
+        "closeOrder": True
+    }
+
+    data_json = json.dumps(data)
+    
+    url = app.base_url + '/api/v1/orders'
+    str_to_sign = str(get_time()) + 'POST' + '/api/v1/orders' + data_json
+
+    headers = get_headers(str_to_sign)
+
+    response = requests.request("post", url=url, headers=headers, data=data_json)
+    print(response.status_code)
+    print(response.json())
 
 
 @app.get("/")
@@ -20,11 +107,27 @@ def read_root():
     return app.last_msg
     # return {"Hello": "World"}
 
+@app.get("/close")
+def closeTrades():
+    close_all_pos()
 
-@app.post("/trade")
-def trade(request: TradeRequest):
-    app.last_msg = request
-    pprint(request)
+@app.post("/action")
+def action(request: TradeRequest):
+    action = request.action
+
+    if action == "long":
+        trade(side="buy", size_usdt=request.size_usdt)
+        print("LONG - {0} - {1}".format(request.msg, request.price))
+    elif action == "short":
+        trade(side="sell", size_usdt=request.size_usdt)
+        print("SHORT - {0} - {1}".format(request.msg, request.price))
+    elif action == "close":
+        close_all_pos()
+        print("CLOSE position (SL/TP) - {0}".format(request.price))
+    else:
+        return "Invalid action: {0}".format(request)
+    
+    pprint(request.model_dump_json())
     return "ok"
 
 
